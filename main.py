0180de46695aa90eb914faa4bd22395f7faa10d8
aaronlow1220 <117196390+aaronlow1220@users.noreply.github.com>
2025-03-13 16:54:43 +0800

    feat: Added waypoints distance

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import os
 import googlemaps
 from dotenv import load_dotenv
+import requests
 
 load_dotenv()
-
-ADDR = "106 台北市大安區敦化南路二段76號16樓"
 
 API_KEY = os.getenv("GOOGLE_MAP_API_KEY")
 
@@ -16,11 +15,49 @@ def getCoordinate(address: str):
     location = geocode_result["geometry"]["location"]
     return location
 
+
 def getAddress(lat: float, lng: float):
     reverse_geocode_result = gmaps.reverse_geocode((lat, lng))[0]
     address = reverse_geocode_result["formatted_address"]
     return address
 
-if __name__ == '__main__':
+
+def getDistance(origin: str, destination: str):
+    distance_matrix = gmaps.distance_matrix(origin, destination)
+    distance = distance_matrix["rows"][0]["elements"][0]["distance"]["text"]
+    return distance
+
+
+def getWaypointsDistance(waypoints: set):
+    url = f"https://routes.googleapis.com/distanceMatrix/v2:computeRouteMatrix"
+
+    waypointsList = [{"waypoint": {"address": place}} for place in places]
+    obj = {
+        "origins": waypointsList,
+        "destinations": waypointsList,
+        "travelMode": "DRIVE",
+        "routingPreference": "TRAFFIC_AWARE",
+    }
+
+    headers = {
+        "Content-Type": "application/json",
+        "X-Goog-Api-Key": API_KEY,
+        "X-Goog-FieldMask": "originIndex,destinationIndex,duration,distanceMeters,status,condition",
+    }
+
+    response = requests.post(url, headers=headers, json=obj)
+    distance = response.json()
+    return distance
+
+
+def main():
+    places = {"台北車站", "台北101", "松山機場", "台北橋"}
+
+    print(getWaypointsDistance(places))
     # print(getCoordinate(ADDR))
-    print(getAddress(25.0291636, 121.5484016))
+    # print(getAddress(25.0291636, 121.5484016))
+    # print(getDistance("台北車站", "台北101"))
+
+
+if __name__ == "__main__":
+    main()
